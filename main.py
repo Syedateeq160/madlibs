8a73412f90037ce6de6e085ffad99f1774b313e1
Syed Ateeq <cs172011@dsu.edu.pk>
2021-03-20 03:44:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+def madlib():
+    body_part = input("Body Part: ")
+    verb = input("Verb: ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    adj5 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun_plural_1 = input("Noun (plural): ")
+    noun_plural_2 = input("Noun (plural): ")
+
+    madlib = f"I love to fight my fears because it's {adj1}! The journey of becoming fearless starts with hope \
+             in your mind and dream in your {body_part}. Through blood, and sweat and {noun_plural_1}, hopefully \
+            it will go again someday. Yes, but first you have to learn to {verb}, it becomes a part of your life \
+            where you can become a super {adj2} hacker. Becoming fearless let's you take control of your {noun1} which \
+            you can create your own personal warzone of {noun_plural_2}, anything from developing {adj3} fear to fighting \
+            it till you die about the {noun2}. You can maybe become the next Fearless Human with extra {adj4} power. \
+            I hope you start your {adj5} journey by coding with Ateeq :) "
+    print(madlib)
+
+
+
+madlib()
